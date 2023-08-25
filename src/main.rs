2414b97fef0e811b0ccaed5ee4c2e765b0a614de
Tom Vercauteren <tomvercaut@gmail.com>
2023-08-25 23:08:13 +0200

    Added a command line argument parser. [#1]
    The parser handles the `path/p` argument from which the DICOM RTPlans
    will be read.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use clap::Parser;
 
+/// Commandline application to list DICOM RTPlan files.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Directory path from which the DICOM RTPlans will be listed.
+    #[arg(short, long)]
+    path: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("path: {}", &args.path);
 }
