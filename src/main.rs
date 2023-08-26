505adf1f9b1922b669a9b68e06cd6c4c81844ba1
Tom Vercauteren <tomvercaut@gmail.com>
2023-08-26 03:10:01 +0200

    Added a command line flag to increase the speed of RTPLAN listing. [#1]

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ struct Args {
     /// Directory path from which the DICOM RTPlans will be listed.
     #[arg(short, long)]
     path: String,
+    /// Speed up the listing by only parsing files that start with RP*.
+    #[arg(short, long)]
+    fast: bool,
 }
 
 fn main() {
     let args = Args::parse();
     println!("path: {}", &args.path);
+    println!("fast: {}", &args.fast);
 }
